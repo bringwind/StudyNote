9a0e5de0f364082cf836f07ac3d775ae8591dc17
bringwind <47776485+bringwind@users.noreply.github.com>
2023-01-26 10:16:29 +0800

    Create readme.m

---
+++ readme.m
@@ -0,0 +1,7 @@
+echo "# StudyNote" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bringwind/StudyNote.git
+git push -u origin main
